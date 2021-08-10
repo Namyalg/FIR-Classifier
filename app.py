f4fad444b241437e239557fca3df6a454ff6d33a
Namyalg <namyalg@gmail.com>
2021-08-10 14:07:49 +0530

    Restructured repository

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #the driver program
 
-from multiapp import MultiApp
 import streamlit as st
-from enter_case import enter_text
-from pdf import upload_file
+from utilities.multiapp import MultiApp
+from utilities.case_text import enter_text
+from utilities.case_pdf import upload_file
 
 app = MultiApp()
 app.add_app("Enter the text of the case content", enter_text)
