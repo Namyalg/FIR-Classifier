fb936b6ee1b3f437f0ee7449df86ff126619a4e3
Namyalg <namyalg@gmail.com>
2021-08-10 15:26:52 +0530

    Added comments to utilities

--- utilities/case_pdf.py
+++ utilities/case_pdf.py
@@ -1,3 +1,8 @@
+'''This file is used to upload the FIR case file,
+   Extract the exact content of the case and
+   provide its classification
+'''
+
 from PIL import Image 
 import pytesseract 
 import sys 
@@ -6,110 +11,70 @@ from pdf2image import convert_from_bytes
 import streamlit as st
 import os
 import pickle
-
+import csv
+#This API is used to translate text from Kannada to English
 from google_trans_new import google_translator
 pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
-#pytesseract.pytesseract.tesseract_cmd = './.apt/usr/bin/tesseract'
-#pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
-#pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-#pytesseract.pytesseract.tesseract_cmd = (
-#    r'/usr/bin/tesseract'
-#)
-import csv
-page_count = 0
-
 def extract_text(file_path):
+    '''
+        This function is used to convert the pdf file to an image
+        and extract the text of the case 
+    '''
     PDF_file = file_path
-    # Store all the pages of the PDF in a variable 
-    pages = convert_from_bytes(PDF_file, output_folder=".") 
-
+    page_count = 0
+    # Store all the pages of the PDF in translated_text variable 
+    pages = convert_from_bytes(PDF_file, output_folder = ".") 
     images = []
     image_counter = 1
-    all_t = ""
-    # Iterate through all the pages stored above 
+    complete_text = ""
     for page in pages:
       filename = "page_" + str(image_counter) + ".jpg"
       st.write(filename)
       page.save(filename, 'JPEG')
       images.append(filename)
-      st.spinner(text='In progress...')
+      st.spinner(text = 'In progress...')
       text = pytesseract.image_to_string(filename, lang="kan")
-      #st.write(text)
-      all_t += text
+      complete_text += text
       image_counter += 1
-
     FIR_starts = 0
     FIR_ends = 0
     counter = 0
-    
-    #st.write("the entire case content is ")
-    #st.write(all_t)
-
-    for line in all_t.splitlines():
+    for line in complete_text.splitlines():
       counter += 1
       if 'ಪ್ರಥಮ ವರ್ತಮಾನ ವರದಿಯ ವಿವರಗಳು' in line:
-        st.write("started")
         FIR_starts = counter
       if 'ತೆಗೆದುಕೊಂಡ ಕ್ರಮ' in line:
         FIR_ends = counter
-    
-    print("THE FIR content is ")
-    
-    FIR_content = (" ".join(all_t.strip("\n").splitlines()[FIR_starts - 1 : FIR_ends]))    
-    print(FIR_content)
+    FIR_content = (" ".join(complete_text.strip("\n").splitlines()[FIR_starts - 1 : FIR_ends]))    
     for img in images:
         os.remove(img)
     return(FIR_content)
 
-
-def translate_row(row):
+def translate_text(row):
     translator = google_translator()
-    a = translator.translate(row, lang_tgt='en')
-    index = a.find("Report")
-    index2 = a.rfind("11")
-    return a[index+6:index2]
+    translated_text = translator.translate(row, lang_tgt='en')
+    start = translated_text.find("Report")
+    end = translated_text.rfind("11")
+    return translated_text[start + 6 : end]
 
 def upload_file():
     st.title("First Information Report Classifier")
     uploaded_file = st.file_uploader("Upload Files", type='pdf')
     if uploaded_file is not None:
         file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
-        st.write("This will take a few minutes to process \n") 
+        st.write("This will take translated_text few minutes to process \n") 
         kannada_text = (extract_text(uploaded_file.getvalue()))
         st.write("The case content in kannada is \n")
         print(kannada_text)
         if kannada_text == "":
-            st.write("Emtpy")
+            st.write("Empty")
         st.write(kannada_text)
         st.write("The case content in english is \n")
-        english_text = translate_row(kannada_text)
-        print(english_text)
+        english_text = translate_text(kannada_text)
         st.write(english_text)
         case_content = english_text
-
         if st.button("Predict"):
             result = classify_utterance(case_content)
             st.success('The case belongs to class {}'.format(result))
-    return 
-
-
-def classify_utterance(utt):
-    # load the vectorizer
-    loaded_vectorizer = pickle.load(open('vectorizer1.pickle', 'rb'))
-
-    # load the model
-    loaded_model = pickle.load(open('classification1.model', 'rb'))
-
-    # make a prediction
-    return(loaded_model.predict(loaded_vectorizer.transform([utt])))
-
-     
-def enter_text():
-    st.title("First Information Report Classifier")
-    case_content = st.text_input("Case Content", "")
-
-    if st.button("Predict"):
-        result = classify_utterance(case_content)
-        st.success('The output is {}'.format(result))
     return 
--- utilities/case_text.py
+++ utilities/case_text.py
@@ -1,3 +1,5 @@
+#This file is used to accept the case text and provide a suitable classification
+
 import pickle
 import streamlit as st
 
@@ -15,9 +17,7 @@ def classify_utterance(utt):
 def enter_text():
     st.title("First Information Report Classifier")
     case_content = st.text_input("Case Content", "")
-
     if st.button("Predict"):
         result = classify_utterance(case_content)
-
         st.success('The case belongs to class {}'.format(result))
     return 
